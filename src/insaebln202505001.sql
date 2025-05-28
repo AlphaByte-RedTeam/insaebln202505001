ba5431d5adf65dc6963f7972bc0eee2e036c20eb
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-05-28 08:15:00 +0700

    initial commit

---
+++ src/insaebln202505001.sql
@@ -0,0 +1,21 @@
+create or replace procedure RPT_insaebln202505001
+(
+    vnama varchar(512),
+	vlokasi varchar(255),
+	vwilayah varchar(255),
+	vemployee varchar(255),
+	vtipeperiode int,
+	vtahun int,
+	vperiode int,
+	vposisi int,
+	vtt int,
+	vuser varchar(255),
+	vreqkey varchar(255),
+	vdate varchar(255),
+	nomorsurat int,
+	vdepolp int,
+	vtxt int,
+	vupdate int
+) language PLvSQL as $$
+
+$$
