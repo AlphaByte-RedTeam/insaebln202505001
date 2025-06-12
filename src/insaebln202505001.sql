182521afaa7765670a3ff2e9491d5d82f3988c85
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-06-12 15:11:42 +0700

    feat(wip): add table hitungsyaratbayar to calculate percentage prestasi tagih

--- src/insaebln202505001.sql
+++ src/insaebln202505001.sql
@@ -450,6 +450,20 @@ if 1=1 then
     group by inkdwilayah,chkdemployee,chkdsite,chkdda,chkdcustomer,chnofaktur1,datgljt
     ;
 
+    perform create local temporary table if not exists hitungsyaratbayar
+    (
+        inkdwilayah int,chkdsite varchar(255),chkdemployee varchar(255),chkdda varchar(255),
+        deTarget dec(25,6),deReal dec(25,6),dePrestasiTagih dec(25,6)
+    ) on commit preserve rows;
+
+    perform insert into hitungsyaratbayar
+    select inkdwilayah,chkdsite,chkdemployee,chkdda,
+    sum(deTarget) deTarget1,sum(deReal) deReal1,
+    case when isnull(deTarget1,0) <= 0 or isnull(deReal1,0) <= 0 then 0 else (deReal1 / deTarget1) end percentPrestag
+    from piutangbulanan
+    group by inkdwilayah,chkdsite,chkdemployee,chkdda
+    ;
+
 end if;
 end;
 $$
